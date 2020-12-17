26f7f949a1b30723b8a55d6beca49269fb9aed37
Maria Vodovoz <mariiavodovoz@gmail.com>
2020-12-16 20:28:18 -0500

    initial commit

---
+++ script.js
@@ -0,0 +1,12 @@
+//using GreenSock is a JavaScript framework for animation
+gsap.to(".animation", {
+  duration: 3,
+  x: 160,
+  ease: "elastic"
+});
+
+gsap.to(".animation2", {
+  duration: 3,
+  x: -160,
+  ease: "elastic"
+});
